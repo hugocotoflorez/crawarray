fb99b7cd6e1c90682d475a0f955f93fa2824f066
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-05-09 18:42:10 +0200

    first commit

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 
 #define BUF_SIZE (1024 * 1024)
-#define LINE_LEFT_PADDING (8)
-#define LINE_SIZE (72)
+#define LINE_SIZE (80)
+#define TAB_SIZE (8)
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
@@ -17,7 +16,6 @@ main(int argc, char *argv[])
         unsigned char buffer[BUF_SIZE];
         ssize_t n;
         int offset;
-        signed char zero_buf[LINE_LEFT_PADDING];
 
         if (argc != 3) {
                 /* TODO: support a single arguent and extract out name from it */
@@ -31,30 +29,47 @@ main(int argc, char *argv[])
                 return 2;
         }
 
-        fdout = open(argv[1], O_RDONLY);
-        if (fdin < 0) {
+        fdout = open(argv[2], O_WRONLY | O_CREAT | O_APPEND, 0600);
+        if (fdout < 0) {
                 close(fdin);
                 perror("open");
-                return 2;
+                return 3;
         }
 
-        memset(zero_buf, 0, LINE_LEFT_PADDING);
 
-        dprintf(fdout, "static signed char[] _data = {\n");
-        while ((n = read(fdout, buffer, sizeof buffer)) > 0) {
-                for (; n > 0; n -= LINE_SIZE) {
-                        write(fdout, zero_buf, LINE_LEFT_PADDING);
-                        write(fdout, buffer, MAX(LINE_SIZE, n));
+        int counter;
+        counter = TAB_SIZE;
+        /* Todo: Use const char* instead of single bytes as numbers */
+        dprintf(fdout, "static signed char _data[] = {\n\t");
+        while ((n = read(fdin, buffer, sizeof buffer)) > 0) {
+                for (offset = 0; offset < n; offset++) {
+                        switch (buffer[offset]) {
+                        case '\\':
+                        case 0 ... 29: // no printable chars
+                                counter += dprintf(fdout, "%d, ", buffer[offset]);
+                                break;
+                        case '\'':
+                                counter += dprintf(fdout, "'\%c', ", buffer[offset]);
+                                break;
+                        default:
+                                counter += dprintf(fdout, "'%c', ", buffer[offset]);
+                                break;
+                        }
+
+                        if (counter >= LINE_SIZE) {
+                                dprintf(fdout, "\n\t");
+                                counter = TAB_SIZE; /* TODO: now it assumes tabs are 8 width */
+                        }
                 }
         }
-        dprintf(fdout, "}\n");
+        dprintf(fdout, "\n};\n");
 
         close(fdin);
         close(fdout);
 
         if (n < 0) {
                 perror("read");
-                return 3;
+                return 4;
         }
 
         return 0;
